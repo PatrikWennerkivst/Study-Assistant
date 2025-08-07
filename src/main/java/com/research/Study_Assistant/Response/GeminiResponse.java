a4eb184dca4ef05b11c90a48eec1ed4340895aa5
Patrik W <166713313+PatrikWennerkivst@users.noreply.github.com>
2025-08-07 22:59:31 +0200

    created class GeminiResponse to create response variabels

--- src/main/java/com/research/Study_Assistant/Response/GeminiResponse.java
+++ src/main/java/com/research/Study_Assistant/Response/GeminiResponse.java
@@ -1,10 +1,12 @@
 package com.research.Study_Assistant.Response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GeminiResponse {
 
     private List<Candidate> candidateList;
