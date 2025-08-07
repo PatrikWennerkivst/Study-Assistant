612ef3d0a28b07c9858753ef7a22b2e1f69fdc5c
Patrik W <166713313+PatrikWennerkivst@users.noreply.github.com>
2025-08-07 22:13:09 +0200

    Creating first prompts for Geminai AI.

--- src/main/java/com/research/Study_Assistant/Service/StudyAssistantService.java
+++ src/main/java/com/research/Study_Assistant/Service/StudyAssistantService.java
@@ -15,5 +15,17 @@ public class StudyAssistantService {
 
     private String buildPrompt(StudyAssistantRequest studyAssistantRequest) {
         StringBuilder promt = new StringBuilder();
+        switch (studyAssistantRequest.getOperation()) {
+            case "summarize":
+                promt.append("Give a concise and clear summary of the following text in a few sentences:\n\n");
+                break;
+            case "suggest":
+                promt.append("Based on the following content: suggest related topics and further reading. Format the response with clear heading and organized bullet points: \n\n");
+            default:
+                throw  new IllegalArgumentException("Unknown Operation: " + studyAssistantRequest.getOperation());
+        }
+
+        promt.append(studyAssistantRequest.getContent());
+        return promt.toString();
     }
 }
