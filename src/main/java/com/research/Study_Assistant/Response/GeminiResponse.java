598f60741e355af4e4ddf6bfa77db824f6a9cce8
Patrik W <166713313+PatrikWennerkivst@users.noreply.github.com>
2025-08-07 22:58:31 +0200

    created class GeminiResponse to create response variabels

--- src/main/java/com/research/Study_Assistant/Response/GeminiResponse.java
+++ src/main/java/com/research/Study_Assistant/Response/GeminiResponse.java
@@ -1,4 +1,23 @@
 package com.research.Study_Assistant.Response;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class GeminiResponse {
+
+    private List<Candidate> candidateList;
+
+    private static class Candidate {
+        private Content content;
+    }
+
+    private static class Content {
+        private List<Part> parts;
+    }
+
+    private static class Part {
+        private String text;
+    }
 }
