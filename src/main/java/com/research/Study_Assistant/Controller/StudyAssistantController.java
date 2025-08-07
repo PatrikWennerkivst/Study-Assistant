4d66f87b847f9b7307b808c67a500cc62281b3ab
Patrik W <166713313+PatrikWennerkivst@users.noreply.github.com>
2025-08-07 21:48:05 +0200

    Creating Project + Git repo

---
+++ src/main/java/com/research/Study_Assistant/Controller/StudyAssistantController.java
@@ -0,0 +1,13 @@
+package com.research.Study_Assistant.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@CrossOrigin(origins = "*")
+public class StudyAssistantController {
+
+
+}
---
+++ src/main/java/com/research/Study_Assistant/Service/StudyAssistantService.java
@@ -0,0 +1,8 @@
+package com.research.Study_Assistant.Service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudyAssistantService {
+}
