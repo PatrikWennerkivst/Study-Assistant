be310c46947efbd35f2bf5e24a9080158a87c312
Patrik W <166713313+PatrikWennerkivst@users.noreply.github.com>
2025-08-07 21:55:20 +0200

    Createad a request class

--- src/main/java/com/research/Study_Assistant/Controller/StudyAssistantController.java
+++ src/main/java/com/research/Study_Assistant/Controller/StudyAssistantController.java
@@ -1,13 +1,23 @@
 package com.research.Study_Assistant.Controller;
 
+import com.research.Study_Assistant.Service.StudyAssistantService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 @CrossOrigin(origins = "*")
+@AllArgsConstructor
 public class StudyAssistantController {
+    private final StudyAssistantService studyAssistantService;
 
+    @PostMapping("/process")
+    public ResponseEntity<String> processContent() {
+        return "";
+    }
 
 }
---
+++ src/main/java/com/research/Study_Assistant/Request/StudyAssistantRequest.java
@@ -0,0 +1,4 @@
+package com.research.Study_Assistant.Request;
+
+public class StudyAssistantRequest {
+}
